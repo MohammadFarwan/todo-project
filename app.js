56d61ff74ffcc07203c4a3d08aafb2059dff2150
Mohammad Farwan <85956258+MohammadFarwan@users.noreply.github.com>
2024-03-20 15:31:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// let name = prompt("Enter your name ?");
+let name = prompt("Enter your name ?");
 
-// let gender = prompt("Enter your gender \"male or female\"");
+let gender = prompt("Enter your gender \"male or female\"");
 
-// let age = prompt("Enter your age ?");
+let age = prompt("Enter your age ?");
 
-// if (age <= 0) {
-//     alert("Your age is less than or equal to zero")
-// }
+if (age <= 0) {
+    alert("Your age is less than or equal to zero")
+}
 
-// let skipWelcome = confirm("Do you want to skip the welcoming message?");
+let skipWelcome = confirm("Do you want to skip the welcoming message?");
 
-// let welcomingMessage = `Hello `;
+let welcomingMessage = `Hello `;
 
-// if (!skipWelcome) {
-//     let title = "";
-//     if (gender.toLowerCase() === "male") {
-//         title = "Mr.";
-//     } else if (gender.toLowerCase() === "female") {
-//         title = "Ms.";
-//     }
-//     alert(`${welcomingMessage}${title}${name}`)
-// } else {
-//     alert(`${welcomingMessage}${name}`)
-// }
+if (!skipWelcome) {
+    let title = "";
+    if (gender.toLowerCase() === "male") {
+        title = "Mr.";
+    } else if (gender.toLowerCase() === "female") {
+        title = "Ms.";
+    }
+    alert(`${welcomingMessage}${title}${name}`)
+} else {
+    alert(`${welcomingMessage}${name}`)
+}
