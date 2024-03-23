a7997da40f0c61fd330ced12957fba08ef380466
MohammadAlfarwan <alfrwan58@gmail.com>
2024-03-23 11:53:52 +0300

    solution for lab 06

--- app.js
+++ app.js
@@ -23,3 +23,30 @@
 // } else {
 //     alert(`${welcomingMessage}${name}`)
 // }
+
+// Lab 06
+
+
+function promptmessage(message){
+    return prompt(message);
+}
+
+function print(element) {
+    console.log(element);
+}
+
+let skydiving = promptmessage("Have you ever tried skydiving?");
+let cooking = promptmessage("Do you enjoy cooking?");
+let watchingMovies = promptmessage("Do you prefer watching movies at home rather than in theaters?");
+
+let answers = [skydiving, cooking, watchingMovies];
+
+for (let i = 0; i < answers.length; i++) {
+    let element = answers[i];
+    if (element == "") {
+        answers[i] = "invalid";
+        element = "invalid";
+    }
+    print(element);
+}
+
